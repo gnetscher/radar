6ef918008e0b3e5fb630d3ce5b8ec2a20ec5b9a3
gnetscher <gnetscher@gmail.com>
2017-03-08 00:06:30 -0800

    finished preprocessing

--- src/radarFeatures.py
+++ src/radarFeatures.py
@@ -1,7 +1,6 @@
 """Calculate features for fall detection from radar based on Su et al.
 "Dopper Radar Fall Activity Detection Using the Wavelet Transform" """
 
-from math import ceil
 import scipy.io as sio
 import numpy as np
 import os.path as osp
@@ -16,7 +15,42 @@ class RadarExtractor:
      
     @staticmethod
     def wavelet_transform(inVal):
+        # use stationary wavelet transform with rbio3.3 kernel as recommended in paper
         return pywt.swt(inVal, 'rbio3.3')
+
+    def calc_frame_energies(self, frameLen=10, shiftLen=2, fallLen=26):
+        """calculate the energy in self.data for frames composed of 10 samples with stride 2
+           then normalize over 2.6s period (eq 5) roughly corresponding to fall length
+           :param: frameLen: number of samples to create 1 frame lasting 1s total
+           :param: shiftLen: the stride length
+        """
+        # calc energies - eq 4
+        numWTLevels = self.yvelWaveCo.shape[0]*self.yvelWaveCo.shape[1]
+        numFrames = (self.yvelWaveCo.shape[2] - frameLen) / (frameLen / shiftLen) + 1
+        energies = np.zeros((numWTLevels, numFrames))
+        for i in range(numWTLevels):
+            for j in range(numFrames):
+                energies[i, j] = self.calc_energy(i, j, frameLen)
+        # normalize - eq 5
+        M = ((fallLen - frameLen) / shiftLen + 1) / 2
+        ipdb.set_trace()
+        normedEnergies = np.zeros((numWTLevels, numFrames))
+        for i in range(numWTLevels):
+            for j in range(numFrames):
+                start = max(j-M, 0)
+                stop  = min(j+M+1, numFrames)
+                if stop - start == 2*M+1:
+                    dividend = sum(energies[i, start:stop])
+                else:
+                    # use edge value for normalizing at edges
+                    if start == 0:
+                        dividend = sum(energies[i, start:stop]) + \
+                                      (2*M+1 - (stop - start))*energies[i, start]
+                    else:
+                        dividend = sum(energies[i, start:stop]) + \
+                                      (2*M+1 - (stop - start))*energies[i, stop]
+                normedEnergies[i, j] = energies[i, j] / dividend  
+        return normedEnergies        
 
     def __init__(self, dataFiles, frameRate=10, scaleFactor=4e-6):
         """pass in one of more data files for future feature extraction
@@ -41,27 +75,13 @@ class RadarExtractor:
         self.data = imsFull[0]/scaleFactor
         self.yvel = self.calc_avg_vel(self.data)
         self.yvelWaveCo = np.array(self.wavelet_transform(self.yvel))
+        self.frameEnergies = self.calc_frame_energies()
 
-    # TODO: true positive should only be the period of 2.5s around the transition from
-    #       any position to on the ground
-    def extract_window_features(self, windowStart=None, windowStop=None, frameRate=10):
+    def extract_features(self, windowStart=None, windowStop=None, frameRate=10):
         """extract features from self.data from windowStart to windowStop
            :param: windowStart: time of start or none if start from data beginning
            :param: windowStop: time of stop or none if stop at data end
         """
-        # calculate all energy values within window
-        # TODO: properly handle windowing
-        # TODO: properly handle advancing .25 seconds for each window (M=7 b/c window=1s)
-        dataWindow = self.data
-        self.calc_energy(1,1,frameRate)
-        for i in range(yvelWaveCo.shape[0]*yvelWaveCo.shape[1]):
-            energyWindow = []
-            for j, im in enumerate(dataWindow):
-                energyWindow.append(calc_energy(i, j))
-    
-        # normalize each energy value
-    
-    
         # create feature vector
    
     def calc_avg_vel(self, data):
@@ -83,29 +103,23 @@ class RadarExtractor:
                 ydiffarr = np.vstack((ydiffarr, np.zeros(ycurr.shape)))
         return yvel
    
-    def calc_energy(self, i, j, frameRate):
+    def calc_energy(self, i, j, frameLen):
         """calculate the energy from equation 4
            :param: i: index of WT coefficients at 2^i 
            :param: j: frame (note a frame is composed of N samples -- there are len(data)/N frames)
         """
         # window size corresponding to 1s (note paper says 0.5, but our frame rate is lower,
         # so I don't want to make the window too small)
-        N = int(frameRate*1)
+        N = frameLen
         # energy calculation for this window
         out = 0 
         hamWin = hamming(N)
         # calculate energy from eq 4
-        ipdb.set_trace()
         for l in range(N):
-            newVal = (hamWin[l]*self.yvelWaveCo[i/2][i%2][l+j*N/2])**2
+            newVal = (hamWin[l]*self.yvelWaveCo[i/2][i%2][l+j*N/5])**2
             out = out + newVal
         return out 
     
-    @staticmethod
-    def normalize_energy(frameRate):
-        """normalize energy from equation 5 over a 2.5s period"""
-        numFrames=int(ceil(2.5*frameRate))
-
 
 # testing code
 if __name__ == '__main__':
