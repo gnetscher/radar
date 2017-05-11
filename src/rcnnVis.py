89c8b08ef6f27acee6b611210c0f7b27c13ef398
nestsense <nestsense@nestsense.com>
2017-05-11 14:41:59 -0700

    quick test

--- src/rcnnVis.py
+++ src/rcnnVis.py
@@ -91,7 +91,7 @@ def visualize_results(totalDet, totalImageFile):
 
 
 if __name__=="__main__":
-  session.authenticate('DeepLearningTeam', 'MrRobo+0')
+  session.authenticate('Nokia', 'nokia')
   videos = get_videos()
   fileList, paths = create_train_txt(videos)
   totalDet, totalIm = get_rcnn_detections(fileList)
